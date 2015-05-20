853ba1dce083ef12edb4269c096bd22fe689e0d3
joaocampinhos <joao.campinhos@gmail.com>
2015-05-20 22:31:03 +0100

    :alien: cenas do outro mundo

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 var Primedice = require('Primedice');
+var Hapi = require('hapi');
 
 var Zé = new Primedice(require('fs').readFileSync('key'));
-
-/*
-   function bet() {
-   Zé.bet(1, 49.5, '<', function(err, result) {
-   if (err) console.log(err);
-   else console.log("Win: "+result.bet.win+" "+result.user.balance);
-   });
-   }
-
-   setInterval(bet, 2000);
-   */
-
-var Hapi = require('hapi');
-var user;
+var user, balance;
 
 var server = new Hapi.Server();
 server.connection({ port: 4000 });
@@ -23,29 +11,80 @@ var io = require('socket.io')(server.listener);
 
 io.on('connection', function (socket) {
 
-  Zé.users(function (err, result) {
-    if (err) console.log(err);
-    else {
-      user = result.user;
-      io.sockets.emit('user', user);
-    }
-  });
+  if (!user) {
+    Zé.users(function (err, result) {
+      if (err) console.log(err);
+      else {
+        user = result.user;
+        balance = user.balance;
+        io.sockets.emit('user', user);
+        params();
+      }
+    });
+  }
 
-  socket.on('burp', function (data) {
-    console.log(data);
-    socket.emit('okok', data);
+  socket.emit('meep');
+  socket.on('morp', function() {
+    console.log('MEEP MORP');
   });
 
 });
 
+var limit = 0;
+var amnt = 0;
+
+function params() {
+  amnt = (balance/Math.pow(2,13) | 0);
+  limit = balance/Math.pow(2,8);
+}
+
 function bet() {
-  Zé.bet(1, 49.5, '<', function(err, result) {
-    if (err) console.log(err);
-    else io.sockets.emit('bet', result);
+  if (losses + wins === 100) process.exit(1);
+  if (amnt > limit) {
+    params();
+  }
+  Zé.bet(amnt, 49.5, '<', function(err, result) {
+    if (err) console.log(err.text);
+    else {
+      stats(result);
+      io.sockets.emit('bet', result);
+    }
   });
 };
 
-setInterval(bet, 10000);
+
+var losses = 0;
+var wins = 0;
+var row = 0;
+var tpro = 0;
+var statistics = {row:{}};
+
+function stats(bet) {
+  balance += bet.bet.profit;
+  tpro += bet.bet.profit;
+  params();
+  if (bet.bet.win){
+    if (row > 0) {
+      if ( row in statistics.row )
+        statistics.row[row]++;
+      else
+        statistics.row[row] = 1;
+    }
+    row = 0;
+    wins++;
+  }
+  else{
+    row++;
+    losses++;
+    amnt = amnt * Math.pow(2,row);
+  }
+  statistics.wins = wins;
+  statistics.losses = losses;
+  statistics.profit = tpro;
+  io.sockets.emit('stats', statistics);
+}
+
+setInterval(bet, 6000);
 
 server.route({
   method: 'GET',
